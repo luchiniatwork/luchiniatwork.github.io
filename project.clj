6e3f055bed0d986cb9199460d06755a6842bb4d9
Tiago Luchini <luchini@work.co>
2018-03-03 18:47:56 -0500

    Up to latest versions

--- project.clj
+++ project.clj
@@ -1,15 +1,17 @@
-(defproject cryogen "0.1.0"
-  :description "Simple static site generator"
-  :url "https://github.com/lacarmen/cryogen"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-devel "1.6.2"]
+(defproject nyc.luchini "0.1.0"
+  :description "My personal website"
+  :url "https://luchini.nyc"
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [ring/ring-devel "1.6.3"]
                  [compojure "1.6.0"]
                  [ring-server "0.5.0"]
                  [cryogen-markdown "0.1.7"]
-                 [cryogen-core "0.1.57"]]
+                 [cryogen-core "0.1.61"]]
+
   :plugins [[lein-ring "0.9.7"]]
+
   :main cryogen.core
+
   :ring {:init cryogen.server/init
          :handler cryogen.server/handler})
