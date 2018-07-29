9a914429da40cc2bd83f9d64d645865442d685f9
Tiago Luchini <luchini@work.co>
2018-07-29 19:05:32 -0400

    Add Clojure resume and a Clojure cover letter

--- resume/generate.sh
+++ resume/generate.sh
@@ -4,7 +4,7 @@ rm *.pdf
 docker run --rm -v $(pwd):/src \
        luchiniatwork/beautiful-md \
        sh -c \
-       "latex -output-format=pdf \"Luchini - Cover Letter.tex\" && latex -output-format=pdf \"Luchini - Resume.tex\""
+       "latex -output-format=pdf \"Luchini - Cover Letter.tex\" && latex -output-format=pdf \"Luchini - Resume.tex\" && latex -output-format=pdf \"Luchini - Clojure Resume.tex\""
 rm *.aux
 rm *.out
-rm *.log
+#rm *.log
